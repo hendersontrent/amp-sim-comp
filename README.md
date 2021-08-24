8eb6dbf31c9e26e31d59a9c60da2ad9f7138562f
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-24 18:00:22 +1000

    Initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # amp-sim-comp
 Compares various guitar amp simulators on their quantitative characteristics.
+
+## Motivation
+
+Many different virtual guitar amplifier simulators exist today. While most musicians and producers *tonally* understand the differences between the characteristics of different amplifiers, little research has been conducted on empirical, quantitative differences between them. This projects seeks to address that gap.
+
+## Included Amplifiers
+
+Currently, analysis includes the following amplifiers which I have paid for:
+
+* Archetype Nolly (Neural DSP) - 4 amplifiers
+* Fortin Nameless (Neural DSP) - 1 amplifier
+* STL Tonality Will Putney (STL) - 5 amplifiers with 3-4 tube settings for each
---
+++ setup.R
@@ -0,0 +1,24 @@
+#----------------------------------------
+# This script sets out to load all the
+# packages and folders necessary for the
+# project
+#----------------------------------------
+
+#----------------------------------------
+# Author: Trent Henderson, 24 August 2021
+#----------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+
+# Create important folders if none exist
+
+if(!dir.exists('data')) dir.create('data')
+if(!dir.exists('data/raw-signals')) dir.create('data/raw-signals')
+if(!dir.exists('data/raw-signals-numeric')) dir.create('data/raw-signals-numeric')
+if(!dir.exists('data/features')) dir.create('data/features')
+if(!dir.exists('DAW')) dir.create('DAW')
+if(!dir.exists('analysis')) dir.create('analysis')
+if(!dir.exists('output')) dir.create('output')
