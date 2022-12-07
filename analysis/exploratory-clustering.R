d8526e10b87562b7d7ee32104591568bfd465d16
Trent <trent.henderson1@outlook.com>
2022-12-07 14:31:14 +1100

    started cluster analysis

---
+++ analysis/exploratory-clustering.R
@@ -0,0 +1,66 @@
+#-----------------------------------------
+# This script sets out to produce an
+# exploratory cluster analysis of the
+# amplifiers based on their feature values
+#
+# NOTE: This script requires setup.R and
+# analysis/catch22.R to have been run
+# first
+#-----------------------------------------
+
+#-----------------------------------------
+# Author: Trent Henderson, 7 December 2022
+#-----------------------------------------
+
+# Load feature matrix
+
+load("data/features/feat_mat.Rda")
+
+# Make dataframe into wide numerical matrix
+
+wide_data <- feat_mat %>%
+  dplyr::select(c(id, names, values)) %>%
+  pivot_wider(id_cols = "id", names_from = "names", values_from = "values") %>%
+  select(-c(id))
+
+#------------- k-means modelling --------------
+
+# Fit k-means models over the range of 1 <= k <= 9
+
+kclusts <-
+  tibble(k = 1:9) %>%
+  mutate(
+    kclust = map(k, ~ kmeans(wide_data, .x)),
+    tidied = map(kclust, tidy),
+    glanced = map(kclust, glance),
+    augmented = map(kclust, augment, wide_data)
+  )
+
+#------------- Extracting results --------------
+
+# Extract separate results using tidy approach presented in https://www.tidymodels.org/learn/statistics/k-means/
+
+clusters <- kclusts %>%
+  unnest(cols = c(tidied))
+
+assignments <- kclusts %>%
+  unnest(cols = c(augmented))
+
+clusterings <- kclusts %>%
+  unnest(cols = c(glanced))
+
+# Use elbow method over total within sums of squares to determine optimal k
+
+p <- clusterings %>%
+  ggplot(aes(x = k, y = tot.withinss)) +
+  geom_line(size = 0.8) +
+  geom_point(size = 2) +
+  labs(x = "k",
+       y = "Total within sums-of-squares") +
+  scale_x_continuous(limits = c(1, 9),
+                     breaks = seq(from = 1, to = 9, by = 1),
+                     labels = seq(from = 1, to = 9, by = 1)) +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank())
+
+print(p)
