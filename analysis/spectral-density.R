070bde05194350a6901cf40fa2b3d8724d0d59c8
Trent <trent.henderson1@outlook.com>
2022-12-13 17:03:28 +1100

    mds plots

--- analysis/spectral-density.R
+++ analysis/spectral-density.R
@@ -63,9 +63,27 @@ fit <- cmdscale(d, eig = FALSE, k = 2)
 p <- as.data.frame(fit) %>%
   rownames_to_column(var = "id") %>%
   inner_join(metadata, by = c("id" = "id")) %>%
+  mutate(keepers = case_when(
+          id == "Neural DSP Tim Henson 1"            ~ id,
+          id == "Neural DSP Tim Henson 2"            ~ id,
+          id == "Neural DSP Petrucci 1"              ~ id,
+          id == "Neural DSP Abasi 1"                 ~ id,
+          id == "Neural DSP Plini 1"                 ~ id,
+          id == "Neural DSP Gojira 3"                ~ id,
+          id == "Neural DSP Rabea 1"                 ~ id,
+          id == "Neural DSP Rabea 2_EL34"            ~ id,
+          id == "Neural DSP Rabea 2_6L6"             ~ id,
+          id == "Neural DSP Fortin Cali Clean"       ~ id,
+          id == "STL Tonality Andy James 3_Lo"       ~ id,
+          id == "STL Tonality Andy James 3_Hi"       ~ id,
+          id == "STL Tonality Lasse Lammert 3"       ~ id,
+          id == "STL Tonality Lasse Lammert 2"       ~ id,
+          id == "STL Tonality Howard benson 2_Clean" ~ id,
+          id == "STL Tonality Howard benson 2_Lead"  ~ id,
+          TRUE                                       ~ "")) %>% # Visual inspection revealed these as outlying cases
   ggplot(aes(x = V1, y = V2)) +
   geom_point(aes(colour = plugin), size = 2.5) +
-  geom_text_repel(aes(label = id, colour = plugin), size = 2, fontface = "bold", max.overlaps = Inf) +
+  geom_text_repel(aes(label = keepers, colour = plugin), size = 2.5, fontface = "bold", max.overlaps = Inf) +
   labs(x = "Coordinate 1",
        y = "Coordinate 2",
        colour = NULL) +
